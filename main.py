43ad2650ee061f9abc93b2c59d6156ed840fed56
kaizer1v <kaizer1v@gmail.com>
2021-12-20 18:16:51 +0530

    typo

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ OUTPUT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '_data')
 CASES_DATA = 'https://data.covid19bharat.org/csv/latest/district_wise.csv'
 COWIN_DATA = 'http://data.covid19bharat.org/csv/latest/cowin_vaccine_data_districtwise.csv'
 
-case_df = pd.read_csv(CASES_DATA)
+cases_df = pd.read_csv(CASES_DATA)
 cowin_df = pd.read_csv(COWIN_DATA)
 
 vad_cases = cases_df[cases_df['District'] == 'Vadodara']
