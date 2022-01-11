07291ad349c6f1f3626d4861345ceea356bbd1d7
kaizer1v <kaizer1v@gmail.com>
2022-01-11 22:54:15 +0530

    script to pull from data.covid19bhart.org for timeseries and cumulative

--- main.py
+++ main.py
@@ -2,13 +2,20 @@ import os
 import pandas as pd
 
 OUTPUT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '_data')
-CASES_DATA = 'https://data.covid19bharat.org/csv/latest/district_wise.csv'
-COWIN_DATA = 'http://data.covid19bharat.org/csv/latest/cowin_vaccine_data_districtwise.csv'
 
-cases_df = pd.read_csv(CASES_DATA)
-cowin_df = pd.read_csv(COWIN_DATA)
+# map url to filename
+DWNLD_MAP = {
+    'https://data.covid19bharat.org/csv/latest/district_wise.csv': 'cumulative',
+    'https://data.covid19bharat.org/csv/latest/districts.csv': 'timeseries'
+    # 'https://data.covid19bharat.org/csv/latest/cowin_vaccine_data_districtwise.csv': 'cowin'
+}
+DISTRICT_NAME = 'Vadodara'
 
-vad_cases = cases_df[cases_df['District'] == 'Vadodara']
-vad_cowin = cowin_df[cowin_df['District'] == 'Vadodara']
 
-cases_df.to_json(os.path.join(OUTPUT_DIR, 'cases.json'), orient='records')
+for url in DWNLD_MAP:
+    print('Generating {} data from {}'.format(DWNLD_MAP[url], url))
+    df = pd.read_csv(url)
+    vad_df = df[df['District'].str.contains(DISTRICT_NAME)]
+    vad_df.to_json(os.path.join(OUTPUT_DIR, '{}.json'.format(DWNLD_MAP[url])), orient='records')
+
+print('DONE!')
