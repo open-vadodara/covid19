4555e972d170b6c9a039deac6c176cf708b28756
Vivek Shrinivasan <kaizer1v@gmail.com>
2022-01-12 14:58:29 +0530

    dont commit & push from here
    commit & push action moved to github actions

--- main.sh
+++ main.sh
@@ -11,15 +11,4 @@ echo "1. Scraping data"
 python main.py
 echo "1. Scraping completed!"
 
-if git status | grep 'new file\|modified'
-then
-    set -e
-    git commit -am "data updated on - $(date)"
-    git remote set-url "$remote_name" "$repo_uri" # includes access token
-    git push --force-with-lease "$remote_name" "$main_branch" "$repo_uri"
-else
-    set -e
-    echo "No changes since last run"
-fi
-
 echo "main.sh end"
