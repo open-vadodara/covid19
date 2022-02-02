d7657d1513fa2538b6ddcc6ecae02d19f9d90b4e
kaizer1v <kaizer1v@gmail.com>
2022-02-02 20:38:30 +0530

    vaccination data

--- README.md
+++ README.md
@@ -10,5 +10,148 @@ Data pulled from covid19bharat.org (an initiative by DataKind Bengaluru)
 https://open-vadodara.github.io/covid19/data/cumulative.json
 
 ### Time Series of Cumulative Cases
-
 https://open-vadodara.github.io/covid19/data/timeseries.json
+
+### Vaccination Data
+https://open-vadodara.github.io/covid19/data/vaccination.json
+
+
+**Sample data format from cowin**
+
+```javascript
+{
+    'topBlock':
+    {
+        'sites':
+        {
+            'total': 27,
+            'govt': 27,
+            'pvt': 0,
+            'today': None
+        },
+        'sessions':
+        {
+            'total': 3802,
+            'govt': 3802,
+            'pvt': 0,
+            'today': None
+        },
+        'registration':
+        {
+            'total': None,
+            'male': None,
+            'female': None,
+            'others': None,
+            'today': None
+        },
+        'vaccination':
+        {
+            'total': 2381681,
+            'male': 1267703,
+            'female': 1097441,
+            'others': 404,
+            'covishield': 2111635,
+            'covaxin': 270046,
+            'sputnik': 0,
+            'zycov': 0,
+            'today': 579,
+            'tot_dose_1': 1210988,
+            'tot_dose_2': 1154562,
+            'tot_pd': 16131,
+            'total_doses': 2381681,
+            'today_dose_one': 18,
+            'today_dose_two': 412,
+            'today_dose_pd': 149,
+            'today_male': 319,
+            'today_female': 260,
+            'today_others': 0,
+            'today_aefi': 0,
+            'aefi': 76,
+            'yesterday_vac': 1082,
+            'yesterday_dose_one': 79,
+            'yesterday_dose_two': 551,
+            'yesterday_dose_pd': 452
+        }
+    },
+    'vaccinationDoneByTime': [
+        {
+            'ts': '2022-01-16 10',
+            'timestamps': '2022-01-16T11:00:00.000Z',
+            'label': '10:00-11:00',
+            'count': 31,
+            'dose_one': 1,
+            'dose_two': 26,
+            'dose_pd': 4
+        },
+        ...
+    ],
+    'last7DaysRegistration': [],
+    'last30DaysAefi': [
+        {
+            'vaccine_date': '2021-12-17',
+            'aefi': 0
+        },
+        ...
+    ],
+    'last5daySessionStatus': [
+        {
+            'session_date': '2021-12-20',
+            'total': '84',
+            'planned': '2',
+            'ongoing': '49',
+            'completed': '33'
+        },
+        ...
+    ],
+    'getBeneficiariesGroupBy': [
+         {
+            'session_site_id': '222254',
+            'title': 'Bhadarwa PHC',
+            'session_site_name': 'Bhadarwa PHC',
+            'total': 12915,
+            'partial_vaccinated': 7365,
+            'totally_vaccinated': 5426,
+            'precaution_dose': 124,
+            'today': 92
+        },
+        ...
+    ],
+    'reportFor': 'State : 11 & District: 155 | 2022-01-16',
+    'aefiPercentage': 0.006,
+    'vaccinationByAge':
+    {
+        'total': 2381681,
+        'vac_15_17': 49837,
+        'vac_18_45': 1302903,
+        'vac_45_60': 645235,
+        'above_60': 383706
+    },
+    'sessionSiteData':
+    {
+        'total_sites': '1102',
+        'govt_sites': '1058',
+        'pvt_sites': '44',
+        'sites_today': 27,
+        'total_sessions': '35230',
+        'govt_sessions': '33839',
+        'pvt_sessions': '1391'
+    },
+    'vaccinationDoneByTimeAgeWise': [
+        {
+            'ts': '2022-01-16 10',
+            'timestamps': '2022-01-16T11:00:00.000Z',
+            'label': '10:00-11:00',
+            'total': 31,
+            'vac_15_17': 0,
+            'vac_18_45': 18,
+            'vac_45_60': 3,
+            'vac_60_above': 10,
+            'rural': 30,
+            'urban': 5
+        },
+        ...
+    ],
+    'timestamp': '2022-01-16 17:30'
+}
+
+```
--- main.py
+++ main.py
@@ -14,54 +14,74 @@ DWNLD_MAP = {
     # 'https://data.covid19bharat.org/csv/latest/cowin_vaccine_data_districtwise.csv': 'cowin'
 }
 DISTRICT_NAME = 'Vadodara'
+COWIN_STATE_ID = 11
+COWIN_DISTR_ID = 155
 
-def cowin_data(curr_date=datetime.datetime.today()):
-    params = {
-        's_id': 11,
-        'd_id': 155,
-        'd': curr_date.strftime("%Y-%m-%d")
-    }
-    base_url = 'https://api.cowin.gov.in/api/v1/reports/v2/getPublicReports?state_id={s_id}&district_id={d_id}&date={d}'.format(**params)
+def data_exists(dt, df):
+    '''
+    check if data exists in the dataframe for a given date
 
-    resp = requests.request('GET', base_url)
-    vad_data = resp.json()
+    :param: <datetime>      - date object to check
+    :param: <pd.DataFrame>  - data frame to check in
 
-    new_data = {
-        'updated_on': curr_date.strftime('%d-%m-%Y'),
-        'total_vaccinations': vad_data['topBlock']['vaccination']['total'],
-        'total_sessions': vad_data['topBlock']['sessions']['total'],
-        'total_sites': vad_data['topBlock']['sites']['total'],
-        'dose_one': vad_data['topBlock']['vaccination']['tot_dose_1'],
-        'dose_two': vad_data['topBlock']['vaccination']['tot_dose_2'],
-        'dose_precautionary': vad_data['topBlock']['vaccination']['tot_pd'],
-        'male': vad_data['topBlock']['vaccination']['male'],
-        'female': vad_data['topBlock']['vaccination']['female'],
-        'others': vad_data['topBlock']['vaccination']['others'],
-        'covaxin': vad_data['topBlock']['vaccination']['covaxin'],
-        'covishield': vad_data['topBlock']['vaccination']['covishield'],
-        'sputnik': vad_data['topBlock']['vaccination']['sputnik'],
-        'aefi': vad_data['topBlock']['vaccination']['aefi'],
-        'zycov': vad_data['topBlock']['vaccination']['zycov'],
-        'vac_18_45': vad_data['vaccinationByAge']['vac_18_45'],
-        'vac_45_60': vad_data['vaccinationByAge']['vac_45_60'],
-        'above_60': vad_data['vaccinationByAge']['above_60']
-    }
+    :returns: <bool>        - True of exists, False otherwise
+    '''
+    pass
 
-    # read old data, append live data & save it back
-    if os.path.exists(VACC_DATA):
-        old_data = pd.read_json(VACC_DATA)
-        updated_data = old_data.append(new_data, ignore_index=True)
-        updated_data.to_json(VACC_DATA, orient='records')
-    else:
-        new_df = pd.DataFrame.from_dict(new_data, orient='index').T
-        new_df.to_json(VACC_DATA, orient='records')
 
-    print('vaccination data is updated!')
+def cowin_data(from_date, to_date):
+    '''
+    Given a range of dates (from & to), extracts vadodara's
+    '''
+    day_count = (to_date - from_date) + datetime.timedelta(days=1)
+    new_data_arr = []
 
-cowin_data()
+    old_df = pd.DataFrame(columns=['updated_on'])
+    if os.path.exists(VACC_DATA) and os.path.getsize(VACC_DATA) != 0:
+        old_df = pd.read_json(VACC_DATA)
+        # old_df['updated_on'] = pd.to_datetime(old_df['updated_on'])
 
-# for url in DWNLD_MAP:
-#     print('Generating {} data from {}'.format(DWNLD_MAP[url], url))
-#     df = pd.read_csv(url)
-#     vad_df = df[df['District'].str.contains(DISTRICT_NAME)]
-#     vad_df.to_json(os.path.join(OUTPUT_DIR, '{}.json'.format(DWNLD_MAP[url])), orient='records')
+    for curr_date in (from_date + datetime.timedelta(n) for n in range(day_count.days)):
+        params = {
+            's_id': COWIN_STATE_ID,
+            'd_id': COWIN_DISTR_ID,
+            'd': curr_date.strftime("%Y-%m-%d")
+        }
+        base_url = 'https://api.cowin.gov.in/api/v1/reports/v2/getPublicReports?state_id={s_id}&district_id={d_id}&date={d}'.format(**params)
+        print('fetching ------>', base_url)
+        resp = requests.request('GET', base_url)
+        vad_data = resp.json()
+        new_data = {
+            'updated_on': curr_date.strftime('%d-%m-%Y'),
+            'total_vaccinations': vad_data['topBlock']['vaccination']['total'],
+            'total_sessions': vad_data['topBlock']['sessions']['total'],
+            'total_sites': vad_data['topBlock']['sites']['total'],
+            'dose_one': vad_data['topBlock']['vaccination']['tot_dose_1'],
+            'dose_two': vad_data['topBlock']['vaccination']['tot_dose_2'],
+            'dose_precautionary': vad_data['topBlock']['vaccination']['tot_pd'],
+            'male': vad_data['topBlock']['vaccination']['male'],
+            'female': vad_data['topBlock']['vaccination']['female'],
+            'others': vad_data['topBlock']['vaccination']['others'],
+            'covaxin': vad_data['topBlock']['vaccination']['covaxin'],
+            'covishield': vad_data['topBlock']['vaccination']['covishield'],
+            'sputnik': vad_data['topBlock']['vaccination']['sputnik'],
+            'aefi': vad_data['topBlock']['vaccination']['aefi'],
+            'zycov': vad_data['topBlock']['vaccination']['zycov'],
+            'vac_18_45': vad_data['vaccinationByAge']['vac_18_45'],
+            'vac_45_60': vad_data['vaccinationByAge']['vac_45_60'],
+            'above_60': vad_data['vaccinationByAge']['above_60']
+        }
+        new_data_arr.append(new_data)
+
+    new_df = pd.DataFrame(new_data_arr)
+    merged_df = pd.concat([old_df, new_df]).drop_duplicates().reset_index(drop=True)
+    merged_df.to_json(VACC_DATA, orient='records')
+
+if __name__ == '__main__':
+    for url in DWNLD_MAP:
+        print('Generating {} data from {}'.format(DWNLD_MAP[url], url))
+        df = pd.read_csv(url)
+        vad_df = df[df['District'].str.contains(DISTRICT_NAME)]
+        vad_df.to_json(os.path.join(OUTPUT_DIR, '{}.json'.format(DWNLD_MAP[url])), orient='records')
+    cowin_data(datetime.date.today(), datetime.date.today())
+
