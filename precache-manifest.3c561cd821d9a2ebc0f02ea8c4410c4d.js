7bbd3ded7334d18308031425a3d751c30fd151b5
kaizer1v <kaizer1v@gmail.com>
2022-02-07 22:14:12 +0530

    Updates

---
+++ precache-manifest.3c561cd821d9a2ebc0f02ea8c4410c4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64d881cdb9af388f540065f210f14bb9",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "a9273dc2930e2af79af7",
+    "url": "/covid19/static/css/main.f5351d2b.chunk.css"
+  },
+  {
+    "revision": "2b4cf7c413781c3fa710",
+    "url": "/covid19/static/js/2.ba943df2.chunk.js"
+  },
+  {
+    "revision": "a9273dc2930e2af79af7",
+    "url": "/covid19/static/js/main.17c1029d.chunk.js"
+  },
+  {
+    "revision": "9edb58dfee5d88d19d35",
+    "url": "/covid19/static/js/runtime~main.60db82c8.js"
+  }
+]);
